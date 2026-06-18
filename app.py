faf155e9ab393e663dd9c68a077a8aadf3db8bc3
JeMaNd-r <53231180+JeMaNd-r@users.noreply.github.com>
2026-06-18 12:56:25 +0200

    Fix syntax typo in dictionary

--- app.py
+++ app.py
@@ -5,13 +5,13 @@
 
 ## Define constants
 LETTER_SCORES = {
-    "AEILNORSTU" = 1,
-    "DG" = ,
-    "BCMP" = 3,
-    "FHVWY" = 4,
-    "K" = 5,
-    "JX" = 8,
-    "QZ" = 10 }
+    "AEILNORSTU": 1,
+    "DG": 2,
+    "BCMP": 3,
+    "FHVWY": 4,
+    "K": 5,
+    "JX": 8,
+    "QZ": 10 }
 
 ## Function to take user input letters
 
