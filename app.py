7053aca5feb9880977801ad30ba423370b729821
JeMaNd-r <53231180+JeMaNd-r@users.noreply.github.com>
2026-06-18 12:51:33 +0200

    Add app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+# App to provide the best-scoring word in Scrabble based on a user-given set of letters
+
+## Load libraries
+
+
+## Define constants
+LETTER_SCORES = {
+    "AEILNORSTU" = 1,
+    "DG" = ,
+    "BCMP" = 3,
+    "FHVWY" = 4,
+    "K" = 5,
+    "JX" = 8,
+    "QZ" = 10 }
+
+## Function to take user input letters
+
+
+
+## Function to match letters to dictonary words
+
+## Function to select highest-scoring word based on point(s) per letter
+
+## Function to return top 5 words
